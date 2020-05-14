8bdc1343f2636eebafee82746e7eb5a26fdcacae
GlassyWing <1490215053@qq.com>
2020-05-14 17:33:13 +0800

    add test

--- deep_sort/sort/kalman_filter.py
+++ deep_sort/sort/kalman_filter.py
@@ -252,7 +252,14 @@ if __name__ == '__main__':
     kf = KalmanFilter()
     mean, covariance = kf.initiate(torch.tensor([10, 15, 0.5, 10]))
     mean, covariance = kf.predict(mean, covariance)
-    # mean, covariance = kf.project(mean, covariance)
     mean, covariance = kf.update(mean, covariance, torch.tensor([12, 20, 0.6, 11]))
-    print(mean)
-    print(covariance)
+
+    mean_2, covariance_2 = kf.initiate(torch.tensor([12, 13, 0.7, 5]))
+    mean_2, covariance_2 = kf.predict(mean_2, covariance_2)
+    mean_2, covariance_2 = kf.update(mean_2, covariance_2, torch.tensor([13, 14, 0.7, 8]))
+
+    squared_maha = kf.gating_distance(torch.cat((mean, mean_2), dim=0),
+                                      torch.cat((covariance, covariance_2), dim=0),
+                                      torch.tensor([[12, 20, 0.6, 11],
+                                                    [20, 16, 0.4, 18]]))
+    print(squared_maha)
